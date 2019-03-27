69aa0262a348d680847905c24f443ae534fbb98a
crsgyn <crsgyn@gmail.com>
2019-03-27 11:09:53 -0300

    componente de testes

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteComponent } from './cliente/cliente.component';
+import { CidadeComponent } from './cidade/cidade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteComponent
+    ClienteComponent,
+    CidadeComponent
   ],
   imports: [
     BrowserModule,
